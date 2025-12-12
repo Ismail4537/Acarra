21dc58d34623f5f10605d6e2ab28d937b2f27a5f
Irsan <irsan.taufik38@gmail.com>
2025-12-12 16:03:00 +0700

    Dokumentasi

--- resources/views/front-page/contact.blade.php
+++ resources/views/front-page/contact.blade.php
@@ -1,5 +1,9 @@
 <x-front-page.layout>
 <x-slot:title> {{$title}} </x-slot:title>
-    
+    {{-- 
+        /**
+        * Tingal isi sesuai kebutuhan halaman 
+        */
+    --}}
     <h1>Halaman contact</h1>
 </x-front-page.layout>
