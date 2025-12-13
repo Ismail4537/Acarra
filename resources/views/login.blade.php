1e2189befb064d0ac6f4aaf5bb516f2781449a97
Dyka21 <andikakhaerulana21@gmail.com>
2025-12-13 19:51:54 +0700

    Feat: Bikin tampilan layout card dan header login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,13 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-slate-50 min-h-screen flex items-center justify-center font-sans p-4">
-    
-    </body>
+
+    <div class="w-full max-w-md bg-white rounded-2xl shadow-xl border border-slate-100 overflow-hidden">
+        <div class="p-8 text-center">
+            <h1 class="text-3xl font-bold text-slate-800 mb-2">Selamat Datang!</h1>
+            <p class="text-slate-500 text-sm">Masukan email dan password </p>
+        </div>
+        </div>
+
+</body>
 </html>
