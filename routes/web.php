c1033c965066bc09621848dc80889cd23e927c9e
Irsan <irsan.taufik38@gmail.com>
2025-12-13 16:23:20 +0700

    Menambahkan Route Event Page

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
 Route::get('/', function () {
     return view('front-page.home', ["title" => "Home"]);
 });
+
+// Rute untuk Index (event.index)
+Route::get('event', [EventController::class, 'index'])->name('event.index');
+// Rute untuk Show (event.show), menggunakan parameter dinamis {id}
+Route::get('event/{slug}', [EventController::class, 'show'])->name('event.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard.home');
