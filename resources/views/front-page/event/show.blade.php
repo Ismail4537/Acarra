cfd5a08d2677249f334b9b2b4dafd1d45b0fa2b1
Ismail4537 <horispararoh457@gmail.com>
2025-12-15 19:19:50 +0700

    feat: add db connection

--- resources/views/front-page/event/show.blade.php
+++ resources/views/front-page/event/show.blade.php
@@ -17,11 +17,11 @@
         <div class="space-y-4 lg:col-start-3">
             
             <div class="relative w-full h-auto rounded-lg overflow-hidden shadow-xl">
-               <img src="{{ $event['foto'] }}" alt="{{ $event['judul'] }}" class="w-full h-full object-cover">
+               <img src="{{ $event['image_path'] }}" alt="{{ $event['title'] }}" class="w-full h-full object-cover">
                  <div class="lg:col-start-3">
             <div class="flex border border-gray-200 rounded-lg overflow-hidden shadow-md">
                 <div class="w-1/2 flex items-center justify-center py-3 px-2 text-gray-600">
-                    {{ $event['harga'] }}
+                    {{ $event['price'] }}
                 </div>
                 <button class="w-1/2 bg-teal-500 hover:bg-teal-600 text-white font-bold py-3 px-2 transition">
                     Beli Tiket
@@ -34,10 +34,10 @@
 
         <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1">
             <h1 class="text-4xl font-bold text-gray-900 mb-1">
-                {{ $event['judul'] }}
+                {{ $event['title'] }}
             </h1>
             <p class="text-lg text-gray-600 mb-6">
-                {{ $event['kategori'] }}
+                {{ $event['category_id'] }}
             </p>
             
             <div class="space-y-4 mb-8">
@@ -45,14 +45,14 @@
                 <div class="flex items-start">
                     <svg class="w-6 h-6 text-gray-500 mr-3 mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.828 0l-4.243-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
                     <div>
-                        <p class="font-semibold">{{ $event['lokasi'] }}</p>
-                        <p class="text-sm text-gray-600">{{ $event['alamat_lengkap'] }}</p>
+                        <p class="font-semibold">{{ $event['location'] }}</p>
+                        <p class="text-sm text-gray-600">{{ $event['location'] }}</p>
                     </div>
                 </div>
 
                 <div class="flex items-center">
                     <svg class="w-6 h-6 text-gray-500 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                    <span class="font-semibold">{{ $event['tanggal'] }}</span>
+                    <span class="font-semibold">{{ $event['start_date_time'] }} / {{ $event['end_date_time'] }}</span>
                 </div>
             </div>
             <h2 class="text-2xl font-bold text-gray-900 mb-3 border-b pb-2">
@@ -60,13 +60,13 @@
             </h2>
             <div class="text-gray-700 space-y-4">
                 <p>
-                    {{ $event['deskripsi'] }}
+                    {{ $event['description'] }}
                 </p>
                 <a href="#" class="text-blue-600 hover:text-blue-800 font-medium">Baca Lebih Banyak</a>
             </div>
             
             <div class="mt-6">
-                <span class="inline-block bg-gray-200 text-gray-700 text-sm px-3 py-1 rounded-full">{{ $event['kategori'] }}</span>
+                <span class="inline-block bg-gray-200 text-gray-700 text-sm px-3 py-1 rounded-full">{{ $event['category_id'] }}</span>
             </div>
         </div>
 
