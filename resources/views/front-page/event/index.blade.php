073c4cfe95192415e46a57afabf299dc9f814559
Irsan <irsan.taufik38@gmail.com>
2025-12-21 19:08:24 +0700

    Perbaiki Style pada Event Page

--- resources/views/front-page/event/index.blade.php
+++ resources/views/front-page/event/index.blade.php
@@ -15,7 +15,7 @@
             </div>
         </div>
 
-    <div class="p-4 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4 justify-items-center">  
+    <div class="p-4 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">  
         @foreach ($listevent as $event)
         <a href="{{ route('event.show', ['slug' => $event['slug']]) }}">
         <div class="border border-gray-300 w-full max-w-xs bg-white rounded-xl shadow-lg overflow-hidden h-full flex flex-col">    
